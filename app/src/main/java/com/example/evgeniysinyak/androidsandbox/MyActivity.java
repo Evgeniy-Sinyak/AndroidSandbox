b6323f8cfb5e7a2ef16a701ac2a9023f25a96b7e
Evgeniy Sinyak <evgeniy.sinyak@teamdev.com>
2014-09-04 23:37:07 +0300

    Added code to test Jenkins Warnings Plugin

--- app/src/main/java/com/example/evgeniysinyak/androidsandbox/MyActivity.java
+++ app/src/main/java/com/example/evgeniysinyak/androidsandbox/MyActivity.java
@@ -97,7 +97,10 @@ public class MyActivity extends Activity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        int unusedVariable = 01;
+
         if (id == R.id.action_settings) {
+            unusedVariable = 10;
             return true;
         }
         return super.onOptionsItemSelected(item);
